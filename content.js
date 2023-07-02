a2c19cf82050c8dcba55be0a24b91f28c9e9f0ac
pom <113845723+pompompur1nn@users.noreply.github.com>
2023-07-02 18:08:21 -0400

    Create content.js

---
+++ content.js
@@ -0,0 +1,106 @@
+// Define the list of banking website domains (40 banks)
+const bankingWebsites = [
+  "bankofamerica.com",
+  "jpmorganchase.com",
+  "wellsfargo.com",
+  "citigroup.com",
+  "usbank.com",
+  "pnc.com",
+  "tdbank.com",
+  "capitalone.com",
+  "usaa.com",
+  "barclays.com",
+  "deutschebank.com",
+  "hsbc.com",
+  "standardchartered.com",
+  "rbc.com",
+  "bnpparibas.com",
+  "lloydsbank.com",
+  "santanderbank.com",
+  "bbva.com",
+  "ubs.com",
+  "creditsuisse.com",
+  "morganstanley.com",
+  "goldmansachs.com",
+  "societegenerale.com",
+  "bocgroup.com",
+  "citi.com",
+  "commbank.com.au",
+  "anz.com",
+  "westpac.com.au",
+  "nab.com.au",
+  "hsbc.co.uk",
+  "barclaycard.co.uk",
+  "bnpfortis.be",
+  "abnamro.nl",
+  "rbs.co.uk",
+  "tdcanadatrust.com",
+  "scotiabank.com",
+  "icbc.com.cn",
+  "bankofchina.com",
+  "hdfcbank.com",
+  "sbi.co.in",
+  "bancopopular.es"
+];
+
+// Define the list of major game website domains (40 games)
+const gameWebsites = [
+  "steamcommunity.com",
+  "epicgames.com",
+  "ubisoft.com",
+  "origin.com",
+  "battle.net",
+  "playstation.com",
+  "xbox.com",
+  "roblox.com",
+  "minecraft.net",
+  "fortnite.com",
+  "leagueoflegends.com",
+  "playvalorant.com",
+  "warthunder.com",
+  "worldofwarcraft.com",
+  "ea.com",
+  "uplay.com",
+  "rockstargames.com",
+  "nintendo.com",
+  "microsoft.com",
+  "sony.com",
+  "bethesda.net",
+  "activision.com",
+  "riotgames.com",
+  "cdprojekt.com",
+  "bungie.net",
+  "gaijin.net",
+  "blizzard.com",
+  "valvesoftware.com",
+  "ubisoftgroup.com",
+  "2k.com",
+  "square-enix.com",
+  "bandainamcoent.com",
+  "sega.com",
+  "capcom.com",
+  "konami.com",
+  "telltalegames.com",
+  "bethesda.com",
+  "ncsoft.com",
+  "daybreakgames.com",
+  "ubi.com",
+  "pearlabyss.com"
+];
+
+// Listen to web requests
+browser.webRequest.onBeforeSendHeaders.addListener(
+  function(details) {
+    // Get the requested domain from the URL
+    const requestedDomain = new URL(details.url).hostname;
+
+    // Check if the requested domain is in the banking websites list or game websites list
+    if (bankingWebsites.includes(requestedDomain) || gameWebsites.includes(requestedDomain)) {
+      // Find and remove any cookies from the request headers
+      const headers = details.requestHeaders.filter(header => header.name.toLowerCase() !== "cookie");
+      return { requestHeaders: headers };
+    }
+  },
+  { urls: ["<all_urls>"] },
+  ["blocking", "requestHeaders"]
+);
